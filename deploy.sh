4955987326e93a502cb2ccb5ed836e15afb6b6b9
szt <soulzterx@googlemail.com>
2024-01-16 22:00:35 +0100

    ci: better deploy script

--- deploy.sh
+++ deploy.sh
@@ -2,11 +2,13 @@
 
 # ensure project root
 cd "$(dirname "$0")"
-# clean old
-rm -rf /srv/http/blog/*
+
 # fetch new
 git pull
 # build it
 hugo --minify
+
+# clean old
+rm -rf /srv/http/blog/*
 # copy it
 cp -r public/* /srv/http/blog/

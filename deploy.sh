fed511cf02957228f4dac4bda989a27fae92687a
szt <soulzterx@googlemail.com>
2024-01-16 19:27:58 +0100

    ci: added automatic deploy feature

---
+++ deploy.sh
@@ -0,0 +1,12 @@
+#/usr/bin/env bash
+
+# ensure project root
+cd "$(dirname "$0")"
+# clean old
+rm -rf /srv/http/0x29a.me/blog/*
+# fetch new
+git pull
+# build it
+hugo --minify
+# copy it
+cp -r public/* /srv/http/0x29a.me/blog/

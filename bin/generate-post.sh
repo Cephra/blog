248f9d07a635c67eb5a88ba1b3c67848da6dd099
Cephra <soulzterx@googlemail.com>
2024-03-25 21:08:01 +0100

    fix: fix the script

--- bin/generate-post.sh
+++ bin/generate-post.sh
@@ -13,12 +13,19 @@ read input_text
 hugo new content $POST
 
 # Pipe the input text to ollama run gemma command and append it to blog post
-ollama run gemma "\
-Write a blog post based on the following text. Ignore everything between the first two lines reading \"+++\".\
-$(cat $POSTFILE) $input_text \
-" >> content/posts/$1.md
-$DESCRIPTION=$(
-    ollama run gemma "Generate a short description of the following blog post. \ 
-    Keep it short and concise. Here's the post: \"$(cat $POSTFILE)\""
+CONTENTPROMPT="\
+    Write a blog post based on the following text, \
+    but ignore everything between the first two occurences of \"+++\". \
+    This is the text: \
+    $(cat $POSTFILE) $input_text \
+"
+echo $CONTENTPROMPT
+ollama run gemma "$CONTENTPROMPT" >> content/posts/$1.md
+
+DESCRPROMPT="Generate a short description of the following blog post.
+Keep it short and concise. Here's the post: \"$(cat $POSTFILE)\""
+echo $DESCRPROMPT
+DESCRIPTION=$(
+    ollama run gemma "$DESCRPROMPT"
 )
 sed -i "s/+++$/summary = $DESCRIPTION&/" $POSTFILE

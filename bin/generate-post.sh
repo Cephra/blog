2a5e18753747291260e9a63800991e08aec4b496
Cephra <soulzterx@googlemail.com>
2024-03-25 21:45:57 +0100

    feat: current state of ai blog gen script

--- bin/generate-post.sh
+++ bin/generate-post.sh
@@ -12,20 +12,33 @@ read input_text
 # Create a new posts based on the first parameter
 hugo new content $POST
 
-# Pipe the input text to ollama run gemma command and append it to blog post
-CONTENTPROMPT="\
-    Write a blog post based on the following text, \
-    but ignore everything between the first two occurences of \"+++\". \
-    This is the text: \
-    $(cat $POSTFILE) $input_text \
-"
-echo $CONTENTPROMPT
-ollama run gemma "$CONTENTPROMPT" >> content/posts/$1.md
+POSTCONTENT=$(cat $POSTFILE)
 
-DESCRPROMPT="Generate a short description of the following blog post.
-Keep it short and concise. Here's the post: \"$(cat $POSTFILE)\""
-echo $DESCRPROMPT
-DESCRIPTION=$(
-    ollama run gemma "$DESCRPROMPT"
-)
-sed -i "s/+++$/summary = $DESCRIPTION&/" $POSTFILE
+# Pipe the input text to ollama run gemma command and append it to blog post
+ollama run gemma <<EOF | cat <(echo) - >> $POSTFILE
+You are supposed to write a blog post based on input.
+The file currently has the following content:
+"$POSTCONTENT"
+Ignore everything inbetween the +++
+Please continue the blog post based on the following input:
+"$input_text"
+EOF
+
+POSTCONTENT=$(cat $POSTFILE)
+ollama run gemma <<EOF
+Write a short description of a blog post.
+The blog post currently has the following content:
+"$POSTCONTENT"
+Ignore everything inbetween the +++.
+EOF
+
+#
+#DESCRPROMPT="Generate a short description of the following blog post.
+#Keep it short and concise. Here's the post: \"$(cat $POSTFILE)\""
+#echo $DESCRPROMPT
+#DESCRIPTION=$(
+#    ollama run gemma "$DESCRPROMPT"
+#)
+#echo $DESCRIPTION
+##sed -i "s/+++$/summary = $DESCRIPTION&/" $POSTFILE
+#

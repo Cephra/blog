ea36d3688793fd89eb59d46c9c6f699a301afa97
Cephra <soulzterx@googlemail.com>
2024-03-26 01:28:01 +0100

    refactor: refactored the generate script a bit

--- bin/generate-post.sh
+++ bin/generate-post.sh
@@ -6,17 +6,21 @@ cd $(dirname $0)/..
 POST=posts/$1.md
 POSTFILE=content/$POST
 
+# Read post
+function read_post()  {
+    POSTHEADER=$(pcregrep -M '^\+\+\+$(\n|.)*^\+\+\+$' $POSTFILE)
+    POSTHEADER_STRIPPED=$(echo "$POSTHEADER" | sed '1d;$d')
+    POSTHEADER_LINES=$(echo $(echo "$POSTHEADER" | wc -l)+1 | bc)
+    POSTCONTENT=$(cat "$POSTFILE" | tail -n +$POSTHEADER_LINES)
+}
+
 # Read input text from the user
 read input_text
 
 # Create a new posts based on the first parameter
 hugo new content $POST
 
-# Read post
-POSTHEADER=$(pcregrep -M '^\+\+\+$(\n|.)*^\+\+\+$' $POSTFILE)
-POSTHEADER_STRIPPED=$(echo "$POSTHEADER" | sed '1d;$d')
-POSTHEADER_LINES=$(echo $(echo "$POSTHEADER" | wc -l)+1 | bc)
-POSTCONTENT=$(cat "$POSTFILE" | tail -n +$POSTHEADER_LINES)
+read_post
 
 # Pipe the input text to ollama run gemma command and append it to blog post
 ollama run gemma <<EOF >> $POSTFILE
@@ -28,7 +32,9 @@ Continue the blog post based on the following input:
 Include everything that was in the post before.
 EOF
 
-POSTCONTENT=$(cat "$POSTFILE" | tail -n +$POSTHEADER_LINES)
+read_post
+
+# Generate description
 POSTDESCRIPTION=$(ollama run gemma <<EOF
 In first person, write a summary, as short as possible, about this blog post:
 "$POSTCONTENT"
@@ -37,6 +43,7 @@ summary = "[summary sentences here]"
 EOF
 )
 
+# Reassemble the post
 cat <<EOF > $POSTFILE
 +++
 $POSTHEADER_STRIPPED

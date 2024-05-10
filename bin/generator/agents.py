c3d8731037851fe2169482aab53f4442563d90b8
Cephra <soulzterx@googlemail.com>
2024-05-10 02:20:20 +0200

    some random stuff

--- bin/generator/agents.py
+++ bin/generator/agents.py
@@ -1,9 +1,10 @@
 import ollama
 
 class BaseAgent():
-    def __init__(self, system_prompt: str, model: str):
+    def __init__(self, system_prompt: str, model: str, usernick: str):
         self._system_prompt = system_prompt
         self._model = model
+        self._usernick = usernick
         self._history = []
     
     def _sys_with_history(self) -> str:
@@ -13,7 +14,7 @@ class BaseAgent():
         } ] + self._history
     
     def chat(self, prompt: str) -> str:
-        print("Human: ", prompt)
+        print("\n# {}\n{}\n".format(self._usernick, prompt))
         messages = self._sys_with_history() + [ {
             'role': 'user',
             'content': prompt
@@ -24,6 +25,11 @@ class BaseAgent():
         return message
 
 if __name__ == '__main__':
-    agent = BaseAgent(system_prompt="You are a helpful assistant.", model="llama3")
-    print(agent.chat("Tell me a joke."))
-    print(agent.chat("Now that was funny!"))
+    agent1 = BaseAgent(system_prompt="You're dave talking to your friend max. Introduce yourself. Be curious, ask questions.", model="llama3", usernick="Max")
+    agent2 = BaseAgent(system_prompt="You're max talking to your friend dave. Introduce yourself. Be curious, ask questions.", model="llama3", usernick="Dave")
+    agent_resp = agent1.chat('Hey Dave, how are you?')
+    user_resp = agent2.chat(agent_resp['content'])
+    for i in range(10):
+        agent_resp = agent1.chat(user_resp['content'])
+        user_resp = agent2.chat(agent_resp['content'])
+    agent1.chat(user_resp['content'])

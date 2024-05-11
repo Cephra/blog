0e4813787d40d4d9a18d498b691ee5f1e44d2167
Cephra <soulzterx@googlemail.com>
2024-05-12 01:21:31 +0200

    agentzzz

--- bin/generator/agents.py
+++ bin/generator/agents.py
@@ -1,10 +1,10 @@
 import ollama
 
 class BaseAgent():
-    def __init__(self, system_prompt: str, model: str, usernick: str):
+    def __init__(self, system_prompt: str, model: str, username: str = "User"):
         self._system_prompt = system_prompt
         self._model = model
-        self._usernick = usernick
+        self._username = username
         self._history = []
     
     def _sys_with_history(self) -> str:
@@ -14,22 +14,23 @@ class BaseAgent():
         } ] + self._history
     
     def chat(self, prompt: str) -> str:
-        print("\n# {}\n{}\n".format(self._usernick, prompt))
-        messages = self._sys_with_history() + [ {
+        print("{}: {}".format(self._username, prompt))
+        self._history.append({
             'role': 'user',
             'content': prompt
-        } ]
-        response = ollama.chat(model=self._model, messages=messages)
+        })
+        response = ollama.chat(model=self._model, messages=self._sys_with_history())
         message = response['message']
         self._history.append(message)
         return message
 
 if __name__ == '__main__':
-    agent1 = BaseAgent(system_prompt="You're dave talking to your friend max. Introduce yourself. Be curious, ask questions.", model="llama3", usernick="Max")
-    agent2 = BaseAgent(system_prompt="You're max talking to your friend dave. Introduce yourself. Be curious, ask questions.", model="llama3", usernick="Dave")
-    agent_resp = agent1.chat('Hey Dave, how are you?')
+    agent1 = BaseAgent(system_prompt="You're dave, an it security specialist talking to your coworker Max. You are both analyzing the origin of an attack on your systems. Try your best to solve it.", model="llama3", username="Max")
+    agent2 = BaseAgent(system_prompt="You're dave, an it security specialist talking to your coworker Dave. You are both analyzing the origin of an attack on your systems. Try your best to solve it.", model="llama3", username="Dave")
+    agent_resp = agent1.chat('Hey Dave, how are you? It\'s me, Max!')
     user_resp = agent2.chat(agent_resp['content'])
-    for i in range(10):
+    for i in range(3):
         agent_resp = agent1.chat(user_resp['content'])
         user_resp = agent2.chat(agent_resp['content'])
-    agent1.chat(user_resp['content'])
+    response = agent1.chat(user_resp['content'])
+    print("{}: {}".format(agent2._username, response['content']))

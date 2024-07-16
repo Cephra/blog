539fb16f9f71d8977ff9eaa6485462bff8eb0b77
Cephra <soulzterx@googlemail.com>
2024-07-16 16:19:36 +0200

    separate class for history

--- bin/generator/agents.py
+++ bin/generator/agents.py
@@ -1,32 +1,34 @@
 import ollama
 
+from history import History
+
 class BaseAgent():
-    def __init__(self, system_prompt: str, model: str, username: str = "User"):
+    def __init__(
+        self, 
+        system_prompt: str,
+        model: str,
+        username: str = "User",
+        history: History = History()
+    ):
         self._system_prompt = system_prompt
         self._model = model
         self._username = username
-        self._history = []
-    
-    def _sys_with_history(self) -> str:
-        return [ {
-            'role': 'system',
-            'content': self._system_prompt
-        } ] + self._history
+        self._history = history
     
     def chat(self, prompt: str) -> str:
         print("{}: {}".format(self._username, prompt))
-        self._history.append({
-            'role': 'user',
-            'content': prompt
-        })
-        response = ollama.chat(model=self._model, messages=self._sys_with_history())
+        self._history.push_message("user", prompt)
+        response = ollama.chat(
+            model=self._model,
+            messages=self._history.get_with_sys(self._system_prompt)
+        )
         message = response['message']
-        self._history.append(message)
+        self._history.push_message_obj(message)
         return message
 
 if __name__ == '__main__':
-    agent1 = BaseAgent(system_prompt="You're dave, an it security specialist talking to your coworker Max. You are both analyzing the origin of an attack on your systems. Try your best to solve it.", model="llama3", username="Max")
-    agent2 = BaseAgent(system_prompt="You're dave, an it security specialist talking to your coworker Dave. You are both analyzing the origin of an attack on your systems. Try your best to solve it.", model="llama3", username="Dave")
+    agent1 = BaseAgent(system_prompt="You're Dave, a right wing person with extreme views on political opinions. You're talking to Max, who is a left wing person with extreme views on politics. You do not agree with Max and want to convince him of your views.", model="llama3", username="Max")
+    agent2 = BaseAgent(system_prompt="You're Max, a left wing person with extreme views on political opinions. You're talking to Dave, who is a right wing person with extreme views on politics. You do not agree with Dave. You try not to let Dave's opinion on you.", model="llama3", username="Dave")
     agent_resp = agent1.chat('Hey Dave, how are you? It\'s me, Max!')
     user_resp = agent2.chat(agent_resp['content'])
     for i in range(3):
---
+++ bin/generator/history.py
@@ -0,0 +1,18 @@
+class History():
+    def __init__(self):
+        self._history = []
+    
+    def get_with_sys(self, sys_prompt: str):
+        return [ {
+            'role': 'system',
+            'content': sys_prompt
+        } ] + self._history
+        
+    def push_message(self, role: str, message: str):
+        self._history.append({
+            'role': role,
+            'content': message
+        })
+
+    def push_message_obj(self, message: dict):
+        self._history.append(message)

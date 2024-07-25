8645a225cf92101b62fcd6661d040d61a74652ce
Cephra <soulzterx@googlemail.com>
2024-07-25 06:39:49 +0200

    set default model to llama3.1

--- bin/generator/generate-post.py
+++ bin/generator/generate-post.py
@@ -9,7 +9,7 @@ if __name__ == "__main__":
     parser.add_argument('postname', help='The name of the blog post')
     parser.add_argument('-s', '--start-line', type=int, default=None, help='The starting line number (optional)')
     parser.add_argument('-e', '--end-line', type=int, default=None, help='The ending line number (optional)')
-    parser.add_argument('-m', '--model', type=str, default='llama3', help='The model to use (optional)')
+    parser.add_argument('-m', '--model', type=str, default='llama3.1', help='The model to use (optional)')
     parser.add_argument('-S', '--skip-summary', action='store_true', help='Skip summary generation (optional)')
     parser.add_argument('-G', '--skip-generate', action='store_true', help='Skip post generation (optional)')
 

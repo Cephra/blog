8735186bb33b47f84a6bda2e4e5f5fd7bf6eddff
Christian Frantzen <soulzterx@googlemail.com>
2024-07-26 15:20:41 +0200

    feat: better interface (#37)
    * implementing cmd chat interface
    
    * set default model to llama3.1
    
    * new post

--- bin/generator/generate-post.py
+++ bin/generator/generate-post.py
@@ -2,18 +2,17 @@
 
 import argparse
 
-from blog_post_generator import BlogPostGenerator
+from interface import BlogGenerationInterface
 
 if __name__ == "__main__":
     parser = argparse.ArgumentParser(description='Blog Post Generator')
     parser.add_argument('postname', help='The name of the blog post')
     parser.add_argument('-s', '--start-line', type=int, default=None, help='The starting line number (optional)')
     parser.add_argument('-e', '--end-line', type=int, default=None, help='The ending line number (optional)')
-    parser.add_argument('-m', '--model', type=str, default='llama3', help='The model to use (optional)')
+    parser.add_argument('-m', '--model', type=str, default='llama3.1', help='The model to use (optional)')
     parser.add_argument('-S', '--skip-summary', action='store_true', help='Skip summary generation (optional)')
     parser.add_argument('-G', '--skip-generate', action='store_true', help='Skip post generation (optional)')
 
     args = parser.parse_args()
-
-    blog_post_generator = BlogPostGenerator(args.postname, model=args.model)
-    blog_post_generator.repl()
+    
+    BlogGenerationInterface(**vars(args)).cmdloop()

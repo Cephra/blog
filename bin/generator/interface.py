07ee0e3cc990bd170eaee75c665f271a9e268f20
Cephra <soulzterx@googlemail.com>
2024-09-03 23:31:49 +0200

    remove logging

--- bin/generator/interface.py
+++ bin/generator/interface.py
@@ -17,24 +17,19 @@ class BlogGenerationInterface(cmd.Cmd):
     
     def do_generate(self, arg):
         'Generate a blog post'
-        print('Generating...')
-        print(arg)
         self._last_response = BlogAgent(model=self._model).chat(arg)["content"]
         
     def do_extend(self, arg):
         'Extend the blog post with more content'
-        print('Extending...')
         self._last_response = ExtendAgent(blog_post=self._blog_post, model=self._model).chat(arg)["content"]
 
     def do_summarize(self, arg):
         'Generate a blog post summary'
-        print('Summarizing...')
         summary = SummaryAgent(model=self._model).chat(self._blog_post.join_content())["content"]
         self._blog_post.update_summary(summary)
 
     def do_continue(self, arg):
         'Create a continuation blog post based on another post'
-        print('Continuing...')
         args = arg.split()
         if len(args) < 1:
             print("you must provide a post name")

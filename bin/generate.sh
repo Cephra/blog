55863c8aca7c9b400a135a18649831dbc3bde329
Cephra <soulzterx@googlemail.com>
2025-01-31 18:04:50 +0100

    chore: general update

--- bin/generate.sh
+++ bin/generate.sh
@@ -1,2 +1,2 @@
 cd "$(dirname "$0")"
-docker compose -f ./generator/compose.yaml run --rm app python generate-post.py "$@"
+docker compose -f ./generator/compose.yaml run --rm app python main.py "$@"
